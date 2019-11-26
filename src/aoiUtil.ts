0cb6f753972214a97c717bb5c96a8a94016db6c0
YamadaAoi <37076519+YamadaAoi@users.noreply.github.com>
2019-11-26 14:38:25 +0800

    pretty

--- src/aoiUtil.ts
+++ src/aoiUtil.ts
@@ -2,6 +2,6 @@ import { aoiparseWindD } from "./parseWindD";
 import { aoidownload } from "./download";
 import { aoidownloadTXT } from "./downloadTXT";
 
-export const  parseWindD = aoiparseWindD
-export const  download = aoidownload
-export const  downloadTXT = aoidownloadTXT
+export const parseWindD = aoiparseWindD;
+export const download = aoidownload;
+export const downloadTXT = aoidownloadTXT;
